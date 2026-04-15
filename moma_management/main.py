9ba71bbafc21166573a76a6d8a6605f45de46c26
SoTrx <11771975+SoTrx@users.noreply.github.com>
2026-04-15 13:29:59 +0000

    chore: Preventing profiling for non api calls

--- moma_management/main.py
+++ moma_management/main.py
@@ -98,6 +98,9 @@ PROFILING = os.getenv("PROFILING", "false").lower() == "true"
 if PROFILING:
     @app.middleware("http")
     async def profile_request(request: Request, call_next):
+        if not request.url.path.startswith("/api/v1"):
+            return await call_next(request)
+
         profiler = Profiler()
         profiler.start()
         await call_next(request)
