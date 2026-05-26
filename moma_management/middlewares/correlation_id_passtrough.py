e09538fc2aef7224cb4b2a1a073da112a23eaa53
SoTrx <11771975+SoTrx@users.noreply.github.com>
2026-05-26 09:24:53 +0000

    fix(middlewares): Add correlation_id even when excption are thrown

--- moma_management/middlewares/correlation_id_passtrough.py
+++ moma_management/middlewares/correlation_id_passtrough.py
@@ -2,6 +2,7 @@ from uuid import uuid4
 
 import structlog
 from fastapi import Request
+from starlette.responses import JSONResponse
 
 logger = structlog.get_logger(__name__)
 
@@ -14,10 +15,16 @@ async def correlation_id_passtrough(request: Request, call_next):
     correlation_id = request.headers.get(
         "x-tracking-correlation") or str(uuid4())
 
+    request.state.correlation_id = correlation_id
     # The correlation_id is required for logs, we can set it in the contextvars for this request here.
     structlog.contextvars.clear_contextvars()
     structlog.contextvars.bind_contextvars(DGCorrelationId=correlation_id)
 
-    response = await call_next(request)
+    try:
+        response = await call_next(request)
+    except Exception as e:
+        logger.exception("Unhandled exception during request processing")
+        response = JSONResponse(status_code=500, content={"detail": str(e)})
+
     response.headers["x-tracking-correlation"] = correlation_id
     return response
